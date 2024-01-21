62e525332934772371269a76be429ce4120efb1b
Tina-Farshadi <tina.fsh83@gmail.com>
2024-01-21 19:21:37 +0330

    The room class has been Updated

--- Hotel/src/Room.java
+++ Hotel/src/Room.java
@@ -1,2 +1,31 @@
 public class Room {
+    private String number;
+    private String counter;
+    private String cost;
+    public boolean IsReserved;
+    public Room(String number,String counter,String cost){
+        this.number=number;
+        this.counter=counter;
+        this.cost=cost;
+    }
+
+    public String getCost() {
+        return cost;
+    }
+
+    public String getCounter() {
+        return counter;
+    }
+
+    public String getNumber() {
+        return number;
+    }
+
+    public boolean isReserved() {
+        return IsReserved;
+    }
+
+    public void setReserved(boolean reserved) {
+        IsReserved = reserved;
+    }
 }
